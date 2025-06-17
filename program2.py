104a689b481239ab39e0ffbaca5dce6e770ba90f
sakshi-gupta-0809 <sakshi.gupta@neutrinotechlabs.com>
2025-06-17 19:39:17 +0530

    programe2

---
+++ program2.py
@@ -0,0 +1,9 @@
+name = input("Enter name")
+age = int(input("Enter age"))
+phone = int(input("Enter phone"))
+address = input("Enter address")
+
+print(f"name: {name}")
+print(f"name: {age}")
+print(f"name: {phone}")
+print(f"name: {address}")
